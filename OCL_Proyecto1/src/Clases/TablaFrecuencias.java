df30027b1d7cbf569387d373f2b8192f3bdd217c
HenryD11703 <3680558330101@ingenieria.usac.edu.gt>
2024-03-08 23:41:32 -0600

    Tabla histograma en consola

--- OCL_Proyecto1/src/Clases/TablaFrecuencias.java
+++ OCL_Proyecto1/src/Clases/TablaFrecuencias.java
@@ -2,6 +2,8 @@ package Clases;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class TablaFrecuencias {
     private String titulo;
@@ -27,36 +29,41 @@ public class TablaFrecuencias {
     }
 
     public void calcularFrecuencias() {
+        // Create a LinkedHashSet to remove duplicates and maintain order
+        Set<Double> uniqueValues = new LinkedHashSet<>(valores);
+
         // Calcular frecuencias absolutas brutas
-        for (Double valor : valores) {
-          fb.add(Collections.frequency(valores, valor));
+        for (Double valor : uniqueValues) {
+            fb.add(Collections.frequency(valores, valor));
         }
-    
+
         // Calcular frecuencias absolutas acumuladas
         fa.add(fb.get(0));
         for (int i = 1; i < fb.size(); i++) {
-          fa.add(fa.get(i - 1) + fb.get(i));
+            fa.add(fa.get(i - 1) + fb.get(i));
         }
-    
+
         // Calcular frecuencias relativas
         for (int i = 0; i < fb.size(); i++) {
-          fr.add((double) fb.get(i) / valores.size());
+            fr.add((double) fb.get(i) / valores.size());
         }
-      }
-    
-      public String toString() {
+
+        // Replace valores with uniqueValues for further processing
+        valores = new ArrayList<>(uniqueValues);
+    }
+
+    public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("===============================\n");
         sb.append(titulo).append("\n");
         sb.append("===============================\n");
         sb.append("Valores | Fb | Fa | Fr\n");
-        sb.append("------- | -- | -- | --\n");
+        sb.append("--------|-----|----|---\n");
         for (int i = 0; i < valores.size(); i++) {
-          sb.append(String.format("%6.2f | %2d | %2d | %6.2f%%\n",
-              valores.get(i), fb.get(i), fa.get(i), fr.get(i) * 100));
+            sb.append(String.format("%6.2f | %2d | %2d | %6.2f%%\n",
+                    valores.get(i), fb.get(i), fa.get(i), fr.get(i) * 100));
         }
         sb.append("===============================\n");
         return sb.toString();
-      }
     }
- 
+}
